ea10948c4b073934e85d35335e9130a0e0272a08
CipiVlad <supercoder.cipi@gmail.com>
2024-01-14 18:22:56 +0100

    tuples

---
+++ section_8_tuples_enums/Tuples.js
@@ -0,0 +1,8 @@
+//creating a Tuple with its type definition
+var myTuple;
+//can assign it but keep the order
+myTuple = [1, 'hi'];
+//not possible
+// myTuple=['hi',1]
+// fixed length of array
+var color = [255, 22, 11];
---
+++ section_8_tuples_enums/Tuples.ts
@@ -0,0 +1,11 @@
+//creating a Tuple with its type definition
+let myTuple: [number, string];
+
+//can assign it but keep the order
+myTuple = [1, 'hi']
+
+//not possible
+// myTuple=['hi',1]
+
+// fixed length of array
+const color: [number, number, number] = [255, 22, 11]
