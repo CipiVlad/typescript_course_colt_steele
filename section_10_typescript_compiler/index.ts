75e15da2bdba12c10049b2219fda85e58039fd99
CipiVlad <supercoder.cipi@gmail.com>
2024-01-17 14:27:04 +0100

    config changes

---
+++ section_10_typescript_compiler/index.ts
@@ -0,0 +1,68 @@
+interface Chicken {
+    breed: string;
+    eggPerWeek: number;
+    age: number;
+    name: string
+}
+
+const norma: Chicken = {
+    breed: "Silkie",
+    eggPerWeek: 1,
+    name: "Norma",
+    age: 1
+}
+
+//run tsc index.ts to compile to index.js
+
+// run tsc --watch index.ts | tsc --w index.ts for listening to changes
+
+//working with multiple files --> farmstand.ts
+//to compile farmstand.ts just run "tsc" to compile any ts files and all
+//proved by: changes made in any file and it compiles to any ".js" file
+
+
+
+//----------------------------
+
+//tsconfig.json
+/*compiler options:
+
+// type
+// what you type in this array will be compiled to .js - File
+"files":[] --- other files will be ignored by ts
+
+//better way:
+create a folder which should include compiled files
+i.e.: "src"
+then place this in tsconfig.json like so:
+
+{
+"include":["src"]
+}
+
+or
+
+{
+"exclude":["dontTouch.ts"]
+}
+
+*/
+
+
+
+
+///--------------------------
+
+// OutDir
+//where ts should compile all js files
+
+
+//common: 
+//create folder "dist"
+// uncomment in tsconfig.json "outDir"
+// run commandline : tsc
+
+
+
+///--------------------------
+//
