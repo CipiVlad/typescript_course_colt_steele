c71760ff3f092edf066b6482200d994506638dfc
CipiVlad <supercoder.cipi@gmail.com>
2024-01-17 15:40:50 +0100

    ts and The DOM

--- section_11_MINI_Project_The_DOM_&_Other_Topics/src/index.js
+++ section_11_MINI_Project_The_DOM_&_Other_Topics/src/index.js
@@ -1,32 +1,18 @@
-function logMessageLive(msg: string): void {
+function logMessageLive(msg) {
     if (msg !== null || msg !== undefined || msg !== '') {
         console.info(msg);
     }
     else {
-        console.error('you need some message, maaan!!!')
+        console.error('you need some message, maaan!!!');
     }
 }
-
-logMessageLive('hello friends i am live in console.log()')
-
-
+logMessageLive('hello friends i am live in console.log()');
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 //TS knows about DOM - Modell
 // but it doesn't know: "Is there something with this id element that exists on the page?"
-
-const btn = document.getElementById("btn");
-
+var btn = document.getElementById("btn");
 console.log(btn);
 //TS Non-Null Assertion Operator 
 //hovering over btn: const btn = HTMLElement | null
 //browser console returns "button#btn" proving it's an Element
-
-
-
-
-
-
-
-
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
