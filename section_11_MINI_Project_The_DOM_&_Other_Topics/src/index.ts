5910bab50ae26cb10e84e42c0e32d9f5d7848612
CipiVlad <102032121+CipiVlad@users.noreply.github.com>
2024-01-17 17:49:14 +0100

    changes made section 11

--- section_11_MINI_Project_The_DOM_&_Other_Topics/src/index.ts
+++ section_11_MINI_Project_The_DOM_&_Other_Topics/src/index.ts
@@ -1,13 +1,13 @@
-function logMessageLive(msg: string): void {
-    if (msg !== null || msg !== undefined || msg !== '') {
-        console.info(msg);
-    }
-    else {
-        console.error('you need some message, maaan!!!')
-    }
-}
+// function logMessageLive(msg: string): void {
+//     if (msg !== null || msg !== undefined || msg !== '') {
+//         console.info(msg);
+//     }
+//     else {
+//         console.error('you need some message, maaan!!!')
+//     }
+// }
 
-logMessageLive('hello friends i am live in console.log()')
+// logMessageLive('hello friends i am live in console.log()')
 
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -15,18 +15,25 @@ logMessageLive('hello friends i am live in console.log()')
 //TS knows about DOM - Modell
 // but it doesn't know: "Is there something with this id element that exists on the page?"
 
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn")// ! --> by adding "!" you guarentee it's an HTML and not null
 
-console.log(btn);
+// console.log(btn);
 //TS Non-Null Assertion Operator 
 //hovering over btn: const btn = HTMLElement | null
 //browser console returns "button#btn" proving it's an Element
+// btn?.addEventListener("click", function () {
+//     alert("clicked")
+// })
+
+//Non-Null Assertion Operator "!" --> guarenteed it's not be null
+// btn?.addEventListener("click", function () {
+//     alert("clicked")
+// })
 
 
 
 
 
-
-
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
